694fabd67b8052e8ff3c656828d29d62a4b9769b
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-04-29 16:33:50 -0600

    third

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -51,7 +51,7 @@ export default class FilterObject extends Component {
     <div className="puzzleBox filterObjectPB">
     <h4> Filter Object </h4>
     <span className="puzzleText"> Original: {JSON.stringify(this.state.employees, null, 10) } </span>
-    <input className="inputLine" onChange={ (e) => this./handleChange(e.target.value) }></input>
+    <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
     <button className="confirmationButton" onClick={ () => this.filterEmployees(this.state.userInput) }> Filter </button>
     <span className="resultsBox filtrerObjectRB"> Filtered: {JSON.stringify(this.state.filteredEmployees, null, 10) } </span>
     </div>
